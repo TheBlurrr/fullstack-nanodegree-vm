5ba458671447f1a748ef4c22d91ef0138841911a
Jonathan Alvarado <Jonathan Alvarado>
2016-03-28 09:22:31 -0700

    Fixed SyntaxError but now getting TypeError

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,9 +3,9 @@
 # 
 
 import time
+import psycopg2
 
 ## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
@@ -15,6 +15,12 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute('''
+      SELECT time, content FROM posts ORDER BY time DESC
+      ''')
+    DB.close()
     posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
     posts.sort(key=lambda row: row['time'], reverse=True)
     return posts
@@ -25,5 +31,11 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=fourm")
+    c = DB.cursor()
+    c.execute('''
+      INSERT INTO posts (content) VALUES ('%s') % content)
+    ''')
+    DB.commit()
+    DB.close()
+    
