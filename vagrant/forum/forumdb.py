8719944c7b01622816823fd4ab9f00004936f1ec
Jonathan Alvarado <Jonathan Alvarado>
2016-03-31 08:43:14 -0700

    Inserted tuple parameter for SQL statements strings

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -33,7 +33,7 @@ def AddPost(content):
     '''
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES ('%s')" % content)
+    c.execute("INSERT INTO posts (content) VALUES (%s)", (content,))
     DB.commit()
     DB.close()
     
